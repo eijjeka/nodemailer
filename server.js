276eb43d9bc52fb5eb2c584ba9abde927ab88c0c
Evgeniy Ignatenko <75755667+eijjeka@users.noreply.github.com>
2023-11-01 10:08:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const nodemailer = require('nodemailer');
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://experiment.biz.ua');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 // Обробник POST-запиту з фронтенду
 app.post('/send-email', (req, res) => {
